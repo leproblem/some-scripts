a38b952f322a4072cd5f71baabce620bdd10d7d5
leproblem <nikitakrgs94@gmail.com>
2022-02-16 07:39:08 +0000

    2 scripts to create and delete 10 folders in /home/

---
+++ createfolders.sh
@@ -0,0 +1,5 @@
+for i in {1..10};
+do
+    mkdir /home/folder$i;
+done
+echo "10 folders in /home has been created!"
---
+++ deletefolders.sh
@@ -0,0 +1,5 @@
+for i in {1..10};
+do
+    rmdir /home/folder$i;
+done
+echo "10 folders in /home has been deleted!"
